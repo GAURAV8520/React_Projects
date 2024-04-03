a118bdc5634b2428c36e5445e309cf9685604efa
Gaurav Deshmukh <136880099+GAURAV8520@users.noreply.github.com>
2024-04-04 00:12:04 +0530

    Update Square.jsx

--- Tic_tac/src/Tic_tac_to/Square.jsx
+++ Tic_tac/src/Tic_tac_to/Square.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Square(props) {
   return (
     <div onClick={props.onClick} className='h-36 w-full flex justify-center items-center border-solid border-8 border-clan-500 rounded'>
-        <h5>{props.value}</h5>
+        <h5 className='text-yellow-200  text-3xl font-bold'>{props.value}</h5>
       
     </div>
   )
